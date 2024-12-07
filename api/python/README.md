f59b7d88a03db28d749824ad4750732e5e834656
Ben Barber <ben@vivum.ai>
2024-12-07 14:01:55 -0500

    update and document python package

--- api/python/README.md
+++ api/python/README.md
@@ -1,25 +1,62 @@
-# Squid Simulation Runner API
+# Squid Worker API
 
-A Python API for running simulations in the squid architecture using ZeroMQ.
+A Python API for running simulations in the Squid architecture.
 
 ## How It Works
 
 The `squid.run(sim_fn)` function starts a simulation loop:
 - Receives an agent from the Squid broker.
 - Passes the agent (as a `dict`) to your simulation function `sim_fn`.
-- Sends the simulation results (as a `dict`) back to the broker.
+- Sends the fitness score and optional additional simulation data (as a `dict`) back to the broker.
 
 The loop terminates when there are no more agents to simulate.
+
+## Installation
+
+To install this package locally, first configure your shell environment:
+
+```sh
+export GITLAB_PACKAGE_TOKEN='<your gitlab personal access token>' # must have 'api' scope
+export SQUID_PACKAGE_URL="https://__token__:$GITLAB_PACKAGE_TOKEN@gitlab.com/api/v4/projects/64429395/packages/pypi/simple"
+```
+
+Then run `pip install squid --index-url $SQUID_PACKAGE_URL`
 
 ## Usage
 
 1. **Define Your Simulation Function**: 
-   Your function must take a single agent `dict` and return a result `dict`.
+   Your function must take a single agent `dict` and return a fitness score `float` and an optional `dict` for additional simulation data. 
+
+   The dict should mirror the `[csv_data]` section in your blueprint file and take the form:
+   ```py
+   {
+      "csv_data_key": [row1, row2, ...],
+      ...
+   }
+   ```
+   where the rows are lists of floats that line up with the headers specified in the blueprint.
+   These rows will be appended to the csv files for the best agent after each generation.
+
+   In other words, your function must match this signature:
 
    ```py
-   def my_simulation(agent: dict) -> dict:
+   def my_simulation(agent: dict) -> tuple[float, dict[str, list[list[float]]] | None]
+   ```
+
+   An example:
+
+   ```py
+   def my_simulation(agent):
        # Process agent and return a result
-       return {"fitness": agent["value"] * 2}
+       fitness = 12.4
+       sim_data = {
+         "sensor_outputs": [
+            [0.1, 0.2, 0.3, 0.4], 
+            [0.5, 0.6, 0.7, 0.8]
+         ]
+       }
+
+       return fitness, sim_data
    ```
 
 2. **Create the entrypoint**: 
@@ -28,20 +65,48 @@ The loop terminates when there are no more agents to simulate.
    ```py
    import squid
 
-   squid.run(my_simulation)
+   ...
+
+   if __name__ == "__main__":
+      squid.run(my_simulation)
    ```
 
-3. **Dockerize**: 
-   More on this later
+## Requirements
 
-## Developers
+- Python 3.11+
+- `pyzmq`
+
+## Development
+
+### Publishing the package
+
+To publish a new version of the package to the GitLab package registry, first configure your shell environment:
+
+```sh
+export WORKDIR="$(pwd)" # root of this repository
+
+export TWINE_USERNAME='__token__'
+export TWINE_PASSWORD='<your gitlab personal access token>' # must have 'api' scope
+export TWINE_REPOSITORY_URL='https://gitlab.com/api/v4/projects/64429395/packages/pypi'
+
+squid-python-publish() {
+  cd $WORKDIR/api/python
+  rm -r dist
+  python -m build
+  twine upload dist/*
+  cd -
+}
+```
+
+Then, bump the version of the package in the `pyproject.toml` file (bump according to semantic versioning).
+
+Then simply run `squid-python-publish`.
+
+### Running it standalone
 
 To use the runner standalone, you will need to set the `SQUID_BROKER_ROUTER_URL` environment variable:
 ```bash
 export SQUID_BROKER_ROUTER_URL="tcp://<broker-address>:<port>"
 ```
 
-## Requirements
-
-- Python 3.11+
-- `pyzmq`
+Additionally, `squid.run()` expects the ID of the experiment to be passed as a command line argument. This is done automatically by the manager process when it spawns the containers, so currently, to use the runner standalone, you will have to start the experiment and then copy the ID and pass it as a command line argument manually.
--- api/python/src/squid/__init__.py
+++ api/python/src/squid/__init__.py
@@ -1,16 +1,31 @@
 import json
 import os
 import struct
+import sys
 from typing import Callable
 import zmq
 
 
-def run(sim_fn: Callable[[dict], dict]):
+def run(sim_fn: Callable[[dict], tuple[float, dict[str, list[list[float]]] | None]]):
     """
     Run the simulation loop.
 
-    :param sim_fn: A callable that takes an agent (dict) and returns a result (dict).
+    This function should be called in the entrypoint of the simulation docker image.
+
+    :param sim_fn:
+        A callable that takes an agent (dict) and returns a fitness score and an optional data dict.
+        The dict should mirror the `[csv_data]` section in your blueprint file and take the form:
+        ```
+        {
+            "csv_data key": [row1, row2, ...],
+            ...
+        }
+        ```
+        where the rows are lists of floats that line up with the headers specified in the blueprint.
+        These rows will be appended to the csv files for the best agent after each generation.
     """
+    exp_id_b = sys.argv[1].encode()
+
     broker_url = os.environ.get("SQUID_BROKER_ROUTER_URL")
     if broker_url is None:
         raise EnvironmentError(
@@ -27,33 +42,82 @@ def run(sim_fn: Callable[[dict], dict]):
 
     try:
         socket.connect(broker_url)
-        socket.send(b"ready")
+        socket.send_multipart([exp_id_b, b"ready"])
 
-        # TODO: sub to broker, poll for a heartbeat, more reliable
+        current_gen = 0
+        best_agent_fitness = 0.0
+        best_agent_data = None
+
+        # TODO: for now shutting down after 5 minutes of no activity
+        # Will want to update this when I implement heartbeating between the
+        # manager and the workers
         while socket.poll(3e5) > 0:
             msgb = socket.recv_multipart()
-            cmd = msgb[0]
-            if cmd == b"sim":
-                agent = json.loads(msgb[2])
-                try:
-                    result = sim_fn(agent)
-                    fitness_b = struct.pack("<d", result["fitness"])
-                    socket.send_multipart(
-                        [
-                            b"done",
-                            msgb[1],
-                            fitness_b,
-                            json.dumps(result).encode("utf-8"),
-                        ]
-                    )
-                    socket.send(b"ready")
-                except Exception as e:
-                    print("Simulation error:", e)
-                    socket.send_multipart([b"error", msgb[1], str(e).encode("utf-8")])
-            elif cmd == b"kill":
-                break
-            else:
-                print("Received invalid command:", cmd)
+
+            cmd = msgb[1]
+            match cmd:
+                case b"sim":
+                    try:
+                        gen: int = struct.unpack("<I", msgb[2])
+                        if gen != current_gen:
+                            best_agent_fitness = 0.0
+                            best_agent_data = None
+                            current_gen = gen
+
+                        agent = json.loads(msgb[4])
+                        fitness, data = sim_fn(agent)
+                        if fitness > best_agent_fitness:
+                            best_agent_fitness = fitness
+                            best_agent_data = data
+                        fitness_b = struct.pack("<d", fitness)
+                        socket.send_multipart(
+                            [
+                                exp_id_b,
+                                b"done",
+                                msgb[2],
+                                msgb[3],
+                                fitness_b,
+                            ]
+                        )
+                        socket.send_multipart([exp_id_b, b"ready"])
+                    except Exception as e:
+                        socket.send_multipart(
+                            [
+                                exp_id_b,
+                                b"error",
+                                msgb[2],
+                                msgb[3],
+                                f"Worker error (sim): {e}".encode(),
+                            ]
+                        )
+                        print("Simulation error:", str(e))
+                case b"moredata":
+                    try:
+                        gen: int = struct.unpack("<I", msgb[2])
+                        if gen != current_gen:
+                            raise ValueError(
+                                f"moredata generation mismatch from broker, expected gen {current_gen}, got gen {gen}"
+                            )
+
+                        data_b = json.dumps(best_agent_data).encode()
+                        socket.send_multipart(
+                            [exp_id_b, b"moredata", msgb[2], msgb[3], data_b]
+                        )
+                    except Exception as e:
+                        socket.send_multipart(
+                            [
+                                exp_id_b,
+                                b"error",
+                                msgb[2],
+                                msgb[3],
+                                f"Worker error (moredata): {e}".encode(),
+                            ]
+                        )
+                        print("Error:", e)
+                case b"kill":
+                    break
+                case _:
+                    print("Received invalid command:", cmd)
     finally:
         socket.close()
         ctx.term()
